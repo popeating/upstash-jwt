7289a95c946b92922551d4ce126a5907146efc21
popeating <pop@popeating.it>
2022-09-02 22:59:11 +0200

    update

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import { Redis } from '@upstash/redis';
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
 
 export default function Home() {
   return <div>hekk</div>;
